0891972901dd77ab4a59f2f6bf9186ec71c17921
Akshay Gupta <akshay.gupta.1018@gmail.com>
2026-01-14 16:42:39 +0530

    Implemented User Dashboard

---
+++ src/pages/UserDashboard.jsx
@@ -0,0 +1,40 @@
+import Navbar from "../components/Navbar";
+import TestCard from "../components/Testcard";
+
+export default function UserDashboard() {
+  return (
+    <div className="h-screen flex flex-col bg-gray-100">
+      
+      <Navbar />
+
+       <div className="flex-1 p-8 overflow-y-auto">
+        <h2 className="text-xl font-bold mb-4">
+          Available Tests
+        </h2>
+
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+          <TestCard title="Java Fundamentals" duration="30" level="Easy" />
+          <TestCard title="React Basics" duration="45" level="Medium" />
+          <TestCard title="DSA Challenge" duration="60" level="Hard" />
+          <TestCard title="Spring Boot" duration="50" level="Medium" />
+          <TestCard title="AI Basics" duration="40" level="Easy" />
+          <TestCard title="System Design" duration="60" level="Hard" />
+          <TestCard title="Java Fundamentals" duration="30" level="Easy" />
+          <TestCard title="React Basics" duration="45" level="Medium" />
+          <TestCard title="DSA Challenge" duration="60" level="Hard" />
+          <TestCard title="Spring Boot" duration="50" level="Medium" />
+          <TestCard title="AI Basics" duration="40" level="Easy" />
+          <TestCard title="System Design" duration="60" level="Hard" />
+          <TestCard title="Java Fundamentals" duration="30" level="Easy" />
+          <TestCard title="React Basics" duration="45" level="Medium" />
+          <TestCard title="DSA Challenge" duration="60" level="Hard" />
+          <TestCard title="Spring Boot" duration="50" level="Medium" />
+          <TestCard title="AI Basics" duration="40" level="Easy" />
+          <TestCard title="System Design" duration="60" level="Hard" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+
